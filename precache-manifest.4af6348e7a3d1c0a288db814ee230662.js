bc2cfb7396dedfcacb6fe86863a9fd541eb4f5bb
yoojin94 <bas8520@naver.com>
2020-10-24 23:55:05 +0900

    Updates

---
+++ precache-manifest.4af6348e7a3d1c0a288db814ee230662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891e3e01dfdbc2f5e6042bf654784e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50a83388bdd7d04e6f37",
+    "url": "/static/css/main.03ebccb3.chunk.css"
+  },
+  {
+    "revision": "ef1e66cffe92ac86da37",
+    "url": "/static/js/2.381124a0.chunk.js"
+  },
+  {
+    "revision": "027111a9ed268a4edf0db6d7c9257874",
+    "url": "/static/js/2.381124a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a83388bdd7d04e6f37",
+    "url": "/static/js/main.6d249cbc.chunk.js"
+  },
+  {
+    "revision": "ccd1ba2cb5f9ef1da594",
+    "url": "/static/js/runtime-main.cca15eba.js"
+  }
+]);
